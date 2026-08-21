a405f48e4076bed5bf9b3a4479bab76749ebd936
briglass <briglass@gmail.com>
2026-08-21 10:30:58 -0700

    google analytics

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Bebas_Neue } from 'next/font/google';
+import Script from 'next/script';
 import './globals.css';
 
 const titleFont = Bebas_Neue({
@@ -21,6 +22,21 @@ export const viewport = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={titleFont.variable}>
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-3D5C2X0LEQ"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-3D5C2X0LEQ');
+          `}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   );
